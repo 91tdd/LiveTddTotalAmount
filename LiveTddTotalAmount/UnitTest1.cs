af75f1bc263f8cbb5ffe64cb5e4a547e6ce784d3
hatelove <jay.hatelove@gmail.com>
2018-03-03 15:48:38 +0800

    pass test: no budgets

--- LiveTddTotalAmount/UnitTest1.cs
+++ LiveTddTotalAmount/UnitTest1.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
 
 namespace LiveTddTotalAmount
 {
     [TestClass]
-    public class UnitTest1
+    public class TotalAmountTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void no_budgets()
         {
+            var repository = Substitute.For<IRepository<Budget>>();
+            repository.GetAll().Returns(new List<Budget>());
+
+            var accouting = new Accouting(repository);
+            var totalAmount = accouting.TotalAmount(new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
+            Assert.AreEqual(0, totalAmount);
         }
+    }
+
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+    }
+
+    public class Budget
+    {
     }
 }
